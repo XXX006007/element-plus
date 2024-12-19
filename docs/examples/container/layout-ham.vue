9eef4eb0a8ab209fb2eabfdfd5190fd015f19275
XXX006007 <xxx006mail@gmail.com>
2024-12-19 14:59:24 +0800

    Update layout-ham.vue

--- docs/examples/container/layout-ham.vue
+++ docs/examples/container/layout-ham.vue
@@ -2,6 +2,14 @@
   <div class="common-layout">
     <el-container>
       <el-header>Header</el-header>
+      <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
       <el-container>
         <el-aside width="200px">Aside</el-aside>
         <el-main>Main</el-main>
